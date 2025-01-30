95ecd6be0686baaa41209bf28ad823eb176e254f
mayarozen <maya.rozen53@gmail.com>
2025-01-30 13:45:02 +0200

    Added missing index.tsx file

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// app/index.tsx
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to My App!</Text>
+    </View>
+  );
+}
